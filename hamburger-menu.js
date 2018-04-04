305fabc832c4d6f12f73d33dac2b612fe89acd4e
nicolashidalgo1989 <38006165+nicolashidalgo1989@users.noreply.github.com>
2018-04-04 13:16:38 -0300

    Update hamburger-menu.js

--- hamburger-menu.js
+++ hamburger-menu.js
@@ -1,14 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.querySelector('.hamburger-menu');  
-    const line = document.createElement('span');
-    line.className = 'line';
-    hamburgerMenu.appendChild(line);   
-    hamburgerMenu.addEventListener('click', (e) => {
-        e.preventDefault();  
-        if(e.target.classList.contains('line')){
-            e.target.classList.toggle('equis');  
-        }else{
-            e.target.children[0].classList.toggle('equis');  
-        };
-    });  
-})
+const hamburgerMenu = document.querySelector('.hamburger-menu');  
+const line = document.createElement('span');
+line.className = 'line';
+hamburgerMenu.appendChild(line);   
+hamburgerMenu.addEventListener('click', (e) => {
+  e.preventDefault();  
+  if(e.target.classList.contains('line')){
+    e.target.classList.toggle('equis');  
+  }else{
+    e.target.children[0].classList.toggle('equis');  
+  }
+});  
